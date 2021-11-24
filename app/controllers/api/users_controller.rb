d56c9c4b71ca3db7a872d7f20b35cd735ecb2cd2
kw-8 <kwu11235813@gmail.com>
2021-11-24 10:37:24 -0800

    Refactored user signup to work with more inputs

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -3,7 +3,7 @@ class Api::UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       login(@user)
-      redirect_to user_url(@user)
+      render :show
     else
       render json: @user.errors.full_messages, status: 422
     end
@@ -23,6 +23,6 @@ class Api::UsersController < ApplicationController
   end
 
   def user_params
-    params.require(:user).permit(:username, :password)
+    params.require(:user).permit(:email, :full_name, :username, :password)
   end
 end
--- frontend/components/session/inputs/signup_inputs.jsx
+++ frontend/components/session/inputs/signup_inputs.jsx
@@ -3,12 +3,10 @@ import React from "react";
 const SignupInputs = props => {
   return (
     <>
-      {/* <label htmlFor="email">Email</label>
-      <input id="email" type="text" onChange={props.update('email')} />
-      
-      <label htmlFor="full_name">Full Name</label>
-      <input id="full_name" type="full_name" onChange={props.update('full_name')} /> */}
-      
+      <input id="email" type="text" placeholder="Email" onChange={props.update('email')} />
+      <br />
+      <input id="full_name" type="text" placeholder="Full Name" onChange={props.update('full_name')} />
+      <br />
       <input id="username" type="text" placeholder="Username" onChange={props.update('username')} />
       <br />
       <input id="password" type="password" placeholder="Password" onChange={props.update('password')} />
