3881226d41cd5a42e5c5eedfdb77a6b51fec98b6
kw-8 <kwu11235813@gmail.com>
2021-11-25 14:30:15 -0800

    Fixed frontend routes

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import {
-  useRouteMatch
+  useRouteMatch,
+  Route
 } from "react-router-dom"
 import {
   AuthRoute,
--- frontend/components/nav/nav_bar.jsx
+++ frontend/components/nav/nav_bar.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Switch } from "react-router";
+import { AuthRoute, ProtectedRoute } from "../../util/route";
 
 class NavBar extends React.Component {
   constructor(props){
@@ -12,13 +14,11 @@ class NavBar extends React.Component {
     this.props.logout();
   }
 
-  render() {
+  render() { 
     return (
       <div id='nav-bar'>
         <h1 className='logo'>Instagran</h1>
         <div id='nav-links'>
-          <Link to='/explore'>home</Link>
-          <button onClick={this.handleLogout}>Logout</button>
         </div>
       </div>
     )
