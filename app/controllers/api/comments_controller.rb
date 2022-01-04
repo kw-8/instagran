f9238b866e27118087c479b16ce7445ee8b85ca4
kw-8 <kwu11235813@gmail.com>
2022-01-04 11:51:41 -0800

    Fix: comment deletion rerender

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -16,7 +16,7 @@ class Api::CommentsController < ApplicationController
   def destroy
     @comment = Comment.find(params[:id])
     @comment.destroy
-    render :index
+    render json: @comment
   end
 
   def comment_params
