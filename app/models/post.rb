ad15e81d9d3a6be1446c4cd07af96c4807fa963c
kw-8 <kwu11235813@gmail.com>
2022-01-07 17:42:25 -0800

    Add custom validation for post iamges, with active storage

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,25 +1,37 @@
 class Post < ApplicationRecord
   validates :poster_id, presence: true
-  # validates pictures exist
+  validate :images_validation
   
   belongs_to :poster,
-    primary_key: :id,
-    foreign_key: :poster_id,
-    class_name: 'User'
-
+  primary_key: :id,
+  foreign_key: :poster_id,
+  class_name: 'User'
+  
   has_many :comments,
-    primary_key: :id,
-    foreign_key: :post_id,
-    class_name: 'Comment',
-    dependent: :destroy
-
+  primary_key: :id,
+  foreign_key: :post_id,
+  class_name: 'Comment',
+  dependent: :destroy
+  
   has_many :likes,
-    foreign_key: :post_id,
-    class_name: 'Like'
-
+  foreign_key: :post_id,
+  class_name: 'Like'
+  
   has_many :likers,
-    through: :likes,
-    source: :liker
-
+  through: :likes,
+  source: :liker
+  
   has_many_attached :images
+
+  def images_validation
+    if images.attached?
+      if ( images.any?{|img| img.byte_size > 5000000} && images.map(&:byte_size).sum > 10000000 )
+        images.purge
+        errors[:base] << 'Too big'
+      elsif images.any?{|img| !img.content_type.starts_with?('image/')}
+        images.purge
+        errors[:base] << 'Wrong format'
+      end
+    end
+  end
 end
