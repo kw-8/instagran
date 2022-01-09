6dcf1d7465ecbac8709637f66721a5a46dde5112
kw-8 <kwu11235813@gmail.com>
2022-01-08 23:06:03 -0800

    Add follows to state

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -3,12 +3,14 @@ import postsReducer from './posts_reducer';
 import usersReducer from './users_reducer';
 import commentsReducer from './comments_reducer';
 import likesReducer from './likes_reducer';
+import followsReducer from './follows_reducer';
 
 const entitiesReducer = combineReducers({
   users: usersReducer,
   posts: postsReducer,
   comments: commentsReducer,
-  likes: likesReducer
+  likes: likesReducer,
+  follows: followsReducer
 })
 
 export default entitiesReducer
