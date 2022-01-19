210f2715f3a38bd4e8455624afc2a488d07b4051
kw-8 <kwu11235813@gmail.com>
2022-01-18 16:44:02 -0800

    Handle dropdown methods on page change

--- frontend/components/dropdown.jsx
+++ frontend/components/dropdown.jsx
@@ -2,14 +2,14 @@
 export const setUpClickAway = (button, menu) => {
   document.addEventListener('click', (e) => {
     if (!e.target.matches(button)) {
-      document.querySelector(menu).setAttribute('style', 'visibility: hidden');
+      if (document.querySelector(menu)) document.querySelector(menu).setAttribute('style', 'visibility: hidden');
     }
   })
 }
 export const removeClickAway = (button, menu) => {
   document.removeEventListener('click', (e) => {
     if (!e.target.matches(button)) {
-      document.querySelector(menu).setAttribute('style', 'visibility: hidden');
+      if (document.querySelector(menu)) document.querySelector(menu).setAttribute('style', 'visibility: hidden');
     }
   })
 }
