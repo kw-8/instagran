3454364a7950249ac4567cf0ade811a54898e88b
kw-8 <kwu11235813@gmail.com>
2022-01-19 15:51:55 -0800

    Change nav bar links to open in new tabs, preventing tab-nabbing

--- frontend/components/nav/nav_bar.jsx
+++ frontend/components/nav/nav_bar.jsx
@@ -59,16 +59,16 @@ class NavBar extends React.Component {
                   <div className="nav-dropdown-menu dropdown">
                     <i className="fa fa-user-circle"></i>
                     <Link to={`/user/${currentUser.id}`}>Profile</Link>
-                    <a href="https://www.linkedin.com/in/katherine-wu-ca/">
+                    <a href="https://www.linkedin.com/in/katherine-wu-ca/" target="_blank" rel="noopener noreferrer">
                       <ion-icon name="logo-linkedin"></ion-icon>
                       LinkedIn
                     </a>
-                    <a href="https://github.com/kw-8">
+                    <a href="https://github.com/kw-8" target="_blank" rel="noopener noreferrer">
                       <i className="fa fa-github-alt"></i>
                       {/* <ion-icon name="logo-github"></ion-icon> */}
                       Github
                     </a>
-                    <a href="https://angel.co/u/katherine-wu-18">
+                    <a href="https://angel.co/u/katherine-wu-18" target="_blank" rel="noopener noreferrer">
                       <i className="fa fa-angellist"></i>
                       Angellist
                     </a>
